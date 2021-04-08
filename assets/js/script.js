d28b07cb5cf8304aaedcccb8ae8a1a32bb7edd3a
tommylu780 <nhoksun340@gmail.com>
2021-04-08 23:13:00 +1000

    sign up and declare API key, get and display current weather, covert temp to Celsius, wind will display Km/h

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,44 @@ var currentHumidity = $("#humidity");
 var currentWindS = $("#wind-speed");
 var currentUV = $("#uv-index");
 var sCity = [];
+
+// API key
+var APIkey = "e714c11c1fa1548077fe1386e9ba5477";
+
+// Current weather
+function currentWeather(city) {
+    // query to get data from server side.
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIkey;
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+    }).then(function(response) {
+        console.log(response);
+        var wIcon = response.weather[0].icon;
+        var iconURL = "https://openweathermap.org/img/wn/" + wIcon + "@2x.png";
+        var date = new Date(response.dt * 1000).toLocaleDateString();
+        // Parse the response for city, date and icon
+        $(currentCity).html(response.name + "(" + date + ")" + "<img src=" + iconURL + ">");
+
+        // Convert to Celsius
+        var tempC = (response.main.temp - 273.15);
+        $(currentTemp).html((tempC).toFixed(2) + "&#8451");
+        // Display Humidity
+        $(currentHumidity).html(response.main.humidity + "%");
+        // Display wind speed
+        var windS = response.wind.speed;
+        var wsKMH = (windS).toFixed(1);
+        $(currentWindS).html(wsKMH * 3.6 + " Km/h");
+    });
+}
+// Display weather
+function displayWeather(event) {
+    event.preventDefault();
+    if (searchCity.val().trim() !== "") {
+        city = searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+
+// Buttons
+$("#search-button").on("click", displayWeather);
