e32ed14f7f34c6612fe88401209cb2543fa8ecc7
tommylu780 <nhoksun340@gmail.com>
2021-04-09 11:51:50 +1000

    add 5 days forecast response and display, fix display looking for 5 days forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ function currentWeather(city) {
 
         // Convert to Celsius
         var tempC = (response.main.temp - 273.15);
-        $(currentTemp).html((tempC).toFixed(2) + "&#8451");
+        $(currentTemp).html((tempC).toFixed(0) + "&#8451");
         // Display Humidity
         $(currentHumidity).html(response.main.humidity + "%");
         // Display wind speed, infomation for wind convert to km/h: https://atoc.colorado.edu/~cassano/wx_calculator/formulas/windConversion.html
@@ -40,6 +40,8 @@ function currentWeather(city) {
         $(currentWindS).html(wsKMH * 3.6 + " Km/h");
         // Display UV index.
         UVIndex(response.coord.lon, response.coord.lat);
+        forecast(response.id);
+
     });
 }
 // Display weather
@@ -59,6 +61,31 @@ function UVIndex(ln, lt) {
         method: "GET"
     }).then(function(response) {
         $(currentUV).html(response.value);
+    });
+}
+// 5 day forecast
+function forecast(cityid) {
+    var dayover = false;
+    var queryforcastURL = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityid + "&appid=" + APIkey;
+    $.ajax({
+        url: queryforcastURL,
+        method: "GET"
+    }).then(function(response) {
+
+        for (i = 0; i < 5; i++) {
+            var date = new Date((response.list[((i + 1) * 8) - 1].dt) * 1000).toLocaleDateString();
+            var iconcode = response.list[((i + 1) * 8) - 1].weather[0].icon;
+            var iconurl = "https://openweathermap.org/img/wn/" + iconcode + ".png";
+            var temp = response.list[((i + 1) * 8) - 1].main.temp;
+            var tempC = (temp - 273.5).toFixed(0);
+            var humidity = response.list[((i + 1) * 8) - 1].main.humidity;
+
+            $("#f-date" + i).html(date);
+            $("#f-img" + i).html("<img src=" + iconurl + ">");
+            $("#f-temp" + i).html(tempC + "&#8451");
+            $("#f-humidity" + i).html(humidity + "%");
+        }
+
     });
 }
 
